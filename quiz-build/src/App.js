6eba6cd6c00582847421a3d048ed4820bbe27725
Luka Licheli <lukalicheli@gmail.com>
2022-04-15 00:44:48 -0700

    Fixed errors except the apollo client and mutations

--- quiz-build/src/App.js
+++ quiz-build/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './pages/Landing';
 import Create from './pages/Create';
+import Login from './pages/Login';
+import Signup from './pages/Singup';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <h1>Quiz Builder</h1>
       <Landing/>
       <Create/>
+      <Login/>
+      <Signup/>
     </div>
   );
 }
